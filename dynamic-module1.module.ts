03851805be91325a2c510ac7bda677ee4c0dc037
Li Wenya Colin <wl98336@Colin-MacBookPro.local>
2019-02-14 10:07:53 +0800

    load dynamic module1 whe click button

--- dynamic-module1.module.ts
+++ dynamic-module1.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DynamicComponent1Component } from './dynamic-component1.component';
+import { DynamicComponentLoaderModule } from '../../dynamic-component-loader/dynamic-component-loader.module';
+
 
 @NgModule({
   declarations: [DynamicComponent1Component],
   imports: [
-    CommonModule
+    CommonModule,
+    DynamicComponentLoaderModule.forChild(DynamicComponent1Component)
   ]
 })
 export class DynamicModule1Module { }
